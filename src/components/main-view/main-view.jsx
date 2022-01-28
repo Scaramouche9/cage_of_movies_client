090f4bc8d64f96ea7a7fe91198827adf3db9ec93
Emily Davies <85586446+Scaramouche9@users.noreply.github.com>
2022-01-28 16:42:15 -0500

    Removed unnecessary render statement
    Removed unnecessary render statement from main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -18,24 +18,6 @@ export class MainView extends React.Component {
   }
 
   render() {
-    return (
-      <div className='main-view'>
-        <div>Pig</div>
-        <div>Willy's Wonderland</div>
-        <div>Prisoners of the Ghostland</div>
-        <div>The Sorcerer's Apprentice</div>
-        <div>Kick-Ass</div>
-        <div>The Wicker Man</div>
-        <div>The Weather Man</div>
-        <div>National Treasure</div>
-        <div>Matchstick Men</div>
-        <div>Wild at Heart</div>
-        <div>Raising Arizona</div>
-      </div>
-    );
-  }
-
-  render() {
     const { movies, selectedMovie } = this.state;
     if (movies.length === 0) return <div className='main-view'>The list is empty!</div>;
       return (
