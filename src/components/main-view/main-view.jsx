9127978226d464b40aaeed6b3817f57e5155e1f5
Emily Davies <85586446+Scaramouche9@users.noreply.github.com>
2022-01-31 14:57:09 -0500

    Added LoginView and imported to MainView
    Added login-view.jsx and imported to main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
   constructor() {
     super();
+    // Initial state is set to null
     this.state = {
       movies: [],
       selectedMovie: null
@@ -30,8 +33,15 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
     const { movies, selectedMovie } = this.state;
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
     if (movies.length === 0) return <div className='main-view'/>;
       return (
         <div className='main-view'>
