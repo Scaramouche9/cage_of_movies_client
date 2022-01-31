3f5227ced5ae6c9b0656e289574fbaa3322367cd
Emily Davies <85586446+Scaramouche9@users.noreply.github.com>
2022-01-31 16:45:23 -0500

    Update movie-card.jsx
    Added PropTypes to movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -8,3 +9,25 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      BirthYear: PropTypes.string.isRequired,
+      DeathYear: PropTypes.string.isRequired
+    }).isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }).isRequired,
+    Rating: PropTypes.string.isRequired,
+    Year: PropTypes.number.isRequired,
+    Featured: PropTypes.bool.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
