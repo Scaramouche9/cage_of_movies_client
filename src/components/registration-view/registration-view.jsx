a2bab8e968f7aa309d18f64e09fdd13aee3633da
Emily Davies <85586446+Scaramouche9@users.noreply.github.com>
2022-02-11 17:13:47 -0500

    Update registration-view.jsx
    Added validation to registration-view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,20 +1,69 @@
 import React, { useState } from "react";
 import { PropTypes } from 'prop-types';
 import { Form, Button, Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
+import axios from "axios";
 
 import './registration-view.scss';
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [birthday, setBirthday] = useState('')
+  const [birthday, setBirthday] = useState('');
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+  const [emailErr, setEmailErr] = useState('');
+  const [birthdayErr, setBirthdayErr] = useState('');
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(username, email, password, birthday);
-  // Send a request to the server for authentication
-  // Then call props.onRegistered(username)
-  props.onRegistered(username);
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr('Username Required');
+      isReq = false;
+    } else if (username.length < 8) {
+      setUsernameErr('Username must be at least 8 characters long');
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr('Password Required');
+      isReq = false;
+    } else if (password.length < 12) {
+      setPasswordErr('Password must be at least 12 characters long');
+      isReq = false;
+    }
+    if (!email) {
+      setEmailErr('Please enter a valid email')
+    } else if (email.indexOf('@') === -1) {
+      setEmailErr('Please enter a valid email')
+      isReq = false;
+    }
+    if (!birthday) {
+      setBirthdayErr('Please enter birthday')
+      isReq = false;
+    }
+    return isReq;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isReq = validate();
+    if (isReq) {
+      console.log(username, password);
+      axios.post('https://cage-of-movies.herokuapp.com/users', {
+        username: username,
+        password: password,
+        email: email,
+        birthday: birthday,
+      })
+        .then(response => {
+          const data = response.data;
+          console.log(data);
+          window.open('/', '_self'); // opens in current tab 
+        })
+        .catch(e => {
+          console.log('error registering this user')
+        });
+      props.onLoggedIn(username);
+    }
 };
 
   return (
